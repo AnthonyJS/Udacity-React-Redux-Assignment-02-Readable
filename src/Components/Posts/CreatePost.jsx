28ce317a161b9d1e112042d195d34574998238c8
Anthony <anthony@residentadvisor.net>
2017-08-21 01:36:30 +0100

    Converting Create Post to stateless functional component

--- src/Components/Posts/CreatePost.jsx
+++ src/Components/Posts/CreatePost.jsx
@@ -3,52 +3,50 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { AddPost, UpdateUnsavedPost } from '../../actions/postActions'
 
-class CreatePost extends Component {
-    changeHandler = event => {
+const CreatePost = props => {
+    const changeHandler = event => {
         const fieldToUpdate = event.target.name
         const newValue = event.target.value
 
-        this.props.updateUnsavedPost(fieldToUpdate, newValue)
+        props.updateUnsavedPost(fieldToUpdate, newValue)
     }
 
-    submitHandler = event => {
+    const submitHandler = event => {
         event.preventDefault()
 
-        this.props.addPost(this.props.fieldData)
+        props.addPost(props.fieldData)
     }
 
-    render() {
-        const { title, body, author } = this.props.fieldData
+    const { title, body, author } = props.fieldData
 
-        return (
-            <div>
-                <form onSubmit={this.submitHandler}>
-                    <input
-                        type="text"
-                        id="title"
-                        name="title"
-                        value={title}
-                        onChange={this.changeHandler}
-                    />
-                    <input
-                        type="text"
-                        id="body"
-                        name="body"
-                        value={body}
-                        onChange={this.changeHandler}
-                    />
-                    <input
-                        type="text"
-                        id="author"
-                        name="author"
-                        value={author}
-                        onChange={this.changeHandler}
-                    />
-                    <input type="submit" value="submitty" />
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div>
+            <form onSubmit={submitHandler}>
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    value={title}
+                    onChange={changeHandler}
+                />
+                <input
+                    type="text"
+                    id="body"
+                    name="body"
+                    value={body}
+                    onChange={changeHandler}
+                />
+                <input
+                    type="text"
+                    id="author"
+                    name="author"
+                    value={author}
+                    onChange={changeHandler}
+                />
+                <input type="submit" value="submit" />
+            </form>
+        </div>
+    )
 }
 
 CreatePost.propTypes = {}
